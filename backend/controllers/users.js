94b65987845163bb238f080662d49381bc07866e
Kochkin Artem <tomsun86@gmail.com>
2021-02-27 18:54:12 +0300

    initial pr-15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -125,7 +125,7 @@ const login = (req, res, next) => {
       const token = jwt.sign(
         { _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' },
       );
-      res.send({ token });
+      res.send({ token: token.toString() });
     })
     .catch(next);
 };
