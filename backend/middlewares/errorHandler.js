0fbbf126a7391bdd96eb68cdf22e82054f4ca3a4
Kochkin Artem <tomsun86@gmail.com>
2021-03-30 17:43:02 +0300

    pr-15 second review

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable consistent-return */
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   if (err.status) {
     return res.status(err.status).send({ message: err.message });
   }
