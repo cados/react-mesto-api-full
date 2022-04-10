5ef8e2e6a4db1af360bceaace0672d1706867190
Kochkin Artem <tomsun86@gmail.com>
2022-04-10 11:13:57 +0300

    fix: restore  app.js

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,29 @@
-import express, { urlencoded, json } from 'express';
-import { connect } from 'mongoose';
-import cors from 'cors';
-import helmet from 'helmet';
-import { login, createUser } from './controllers/users';
-import usersRouter from './routes/users';
-import cardsRouter from './routes/cards';
-import { validateLogin, validateUser } from './middlewares/validation';
-import { requestLogger, errorLogger } from './middlewares/logger';
-import errorHandler from './middlewares/errorHandler';
-import { NotFoundError } from './errors';
-
-require('dotenv').config();
+const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const { login, createUser } = require('./controllers/users');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { validateLogin, validateUser } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+const { NotFoundError } = require('./errors');
+require('dotenv').config();
 
 const { PORT = 3001 } = process.env;
 
-connect(process.env.DB_CONNECT, {
+mongoose.connect(process.env.DB_CONNECT, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.use(cors());
 app.use(helmet());
-app.use(urlencoded({ extended: true }));
-app.use(json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
